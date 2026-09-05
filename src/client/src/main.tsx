ab50260aee0557cc26121bca2a0ef7da8fe3b209
anton golovachev <anton.golo.fh@gmail.com>
2026-09-05 23:57:47 +0300

    fix RTL css

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -5,13 +5,28 @@ import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./stores/groceriesStore.ts";
+import {CacheProvider} from '@emotion/react';
+import createCache from '@emotion/cache';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import rtlPlugin from '@mui/stylis-plugin-rtl';
+import {prefixer} from 'stylis';
+
+const theme = createTheme({direction: 'rtl'});
+const rtlCache = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
+        <CacheProvider value={rtlCache}>
+            <ThemeProvider theme={theme}>
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <App/>
+                    </BrowserRouter>
+                </Provider>
+            </ThemeProvider>
+        </CacheProvider>
     </StrictMode>,
 )
